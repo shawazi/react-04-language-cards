2116f7841c7645bc4057acb9fbef2c4374395d05
shawazi <shawazisonfire@gmail.com>
2023-03-02 22:20:38 -0500

    deleted default readme, trying to find correct path for some imports

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import data from "../../helper/data";
+
+console.log(data)
+
+const Cards = () => {
+    return (
+        <div><p>hello</p></div>
+    )
+}
+
+export default Cards;
